e921a33c3122d8a67c08b6f88454669e4ae9d9e4
Святослав <zutkijvoron44@gmail.com>
2023-12-24 18:51:43 +0600

    созданна программа

--- Program.cs
+++ Program.cs
@@ -2,3 +2,27 @@
 using static System.Console;
 
 
+string[] stringSearch(string[] arr)
+{
+    string[] str = new string[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            Array.Resize(ref str, str.Length + 1);
+            str[str.Length - 1] = arr[i];
+        }
+    }
+    return str;
+}
+
+
+int count = ReadInt("введите количество слов");
+string[] arrstr = new string[count];
+for (int i = 0; i < arrstr.Length; i++)
+{
+    WriteLine($"введите {i + 1} слово");
+    arrstr[i] = ReadLine()!;
+}
+string output = string.Join(", ", stringSearch(arrstr));
+WriteLine($"[{output}]");
